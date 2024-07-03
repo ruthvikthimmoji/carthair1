b92c43fc6f38e586957eb818d5a38f74bcef8fd5
Ruthviikthimmoji <149005776+ruthvikthimmoji@users.noreply.github.com>
2024-07-03 20:38:03 +0530

    edit forms updated

--- components/EditCustomersForm.jsx
+++ components/EditCustomersForm.jsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useState } from 'react'
 import { useRouter } from 'next/navigation';
-import { GrUpdate } from 'react-icons/gr';
+import Image from 'next/image';
 
 export default function EditCustomersForm({ id, customer }) {
     const [newCustomer, setNewCustomer] = useState(customer);
@@ -28,32 +28,68 @@ export default function EditCustomersForm({ id, customer }) {
     }
 
     return (
-        <div>
-            <div>
-                <h1 className='flex justify-center glow-text text-4xl font-thin p-10'>EditCustomersForm</h1>
-                <div className=' flex justify-center items-center p-10'>
-                    <form onSubmit={handleSubmit} className=' flex flex-col justify-center border-8 border-orange-200 rounded-lg px-4 py-2'>
-                        <div className='flex flex-col justify-center  shadow-lg shadow-black rounded-lg px-4 py-2 m-4 '>
-                            <label className='p-2 text-orange-400' > Customer Name : </label>
-                            <input className='border border-orange-400 text-orange-400 rounded-lg px-6 py-2 m-2 bg-transparent' onChange={(e) => setNewCustomer({ ...newCustomer, name: e.target.value })} value={newCustomer.name} type='text' />
-                            <label className='p-2  text-orange-400'> Phone Number : </label>
-                            <input className='border border-orange-400 text-orange-400 rounded-lg px-6 py-2 m-2 bg-transparent' onChange={(e) => setNewCustomer({ ...newCustomer, phonenumber: e.target.value })} value={newCustomer.phonenumber}
-                                type="tel" id="phone" name="phone" />
-                            <label className='p-2  text-orange-400'> Date :</label>
-                            <input className='border border-orange-400 text-orange-400 rounded-lg px-6 py-2 m-2 bg-transparent' onChange={(e) => setNewCustomer({ ...newCustomer, date: e.target.value })} value={newCustomer.date}
-                                type='Date' placeholder='dd/mm/yyyy' />
-                            <label className='p-2 text-orange-400'> Services :</label>
-                            <input className='border border-orange-400 text-orange-400 rounded-lg px-6 py-2 m-2 bg-transparent' onChange={(e) => setNewCustomer({ ...newCustomer, services: e.target.value })} value={newCustomer.services}
-                                type='text' />
-                            <label className='p-2  text-orange-400'> Attendant : </label>
-                            <input className='border border-orange-400 text-orange-400 rounded-lg px-6 py-2 m-2 bg-transparent' onChange={(e) => setNewCustomer({ ...newCustomer, attendant: e.target.value })} value={newCustomer.attendant}
-                                type='text' />
-                            <button className="mt-6 mb-4 px-4 py-2 rounded-md bg-orange-500 flex flex-row justify-center items-center rounded-badge">
-                                <span className='mr-2 text-gray-800'>Update</span> <GrUpdate className='text-gray-800' /> </button>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    )
+        <div className='flex flex-col lg:flex-row h-screen'>
+          <div className='flex-1 relative'>
+        <Image
+          src='/2.svg'
+          alt='404'
+          width={1080}
+          height={1920}
+          className='w-full h-full object-cover'
+        />
+      </div>
+      <div className='flex-1 flex flex-col items-center justify-center p-4 lg:p-10'>
+        <h1 className='text-4xl font-thin glow-text mb-4 lg:mb-6 p-6'> EDIT CUSTOMERS</h1>
+        <form onSubmit={handleSubmit}
+          className='w-full max-w-sm backdrop-blur-0 shadow-[0_0_5px_0] p-6 lg:p-16 rounded-lg'>
+          <div className='mb-4'>
+            <label className='block text-orange-200 text-sm mb-2' htmlFor='tel'>
+              Customer Name
+            </label>
+            <input
+              className='shadow border rounded w-full py-2 px-3 text-orange-400 leading-tight bg-transparent border-orange-400 focus:outline-none focus:shadow-outline'
+              onChange={(e) => setNewCustomer({ ...newCustomer, name: e.target.value })} value={newCustomer.name} type='text'
+            />
+          </div>
+          <div className='mb-6'>
+            <label className='block text-orange-200 text-sm mb-2' htmlFor='password'>
+              Phone Number
+            </label>
+            <input className='shadow appearance-none border rounded w-full py-2 px-3 text-orange-400 mb-3 leading-tight bg-transparent border-orange-400 focus:outline-none focus:shadow-outline'
+              onChange={(e) => setNewCustomer({ ...newCustomer, phonenumber: e.target.value })} value={newCustomer.phonenumber}
+              type="tel" id="phone" name="phone" />
+          </div>
+          <div className='mb-6'>
+            <label className='block text-orange-200 text-sm mb-2' htmlFor='password'>
+              Date
+            </label>
+            <input className='shadow appearance-none border rounded w-full py-2 px-3 text-orange-400 mb-3 leading-tight bg-transparent border-orange-400 focus:outline-none focus:shadow-outline'
+              onChange={(e) => setNewCustomer({ ...newCustomer, date: e.target.value })} value={newCustomer.date}
+              type='Date' placeholder='dd/mm/yyyy' />
+          </div>
+          <div className='mb-6'>
+            <label className='block text-orange-200 text-sm mb-2' htmlFor='password'>
+              Services
+            </label>
+            <input className='shadow appearance-none border rounded w-full py-2 px-3 text-orange-400 mb-3 leading-tight bg-transparent border-orange-400 focus:outline-none focus:shadow-outline'
+              onChange={(e) => setNewCustomer({ ...newCustomer, services: e.target.value })} value={newCustomer.services}
+              type='text' />
+          </div>
+          <div className='mb-6'>
+            <label className='block text-orange-200 text-sm mb-2' htmlFor='password'>
+            Attendant
+            </label>
+            <input className='shadow appearance-none border rounded w-full py-2 px-3 text-orange-400 mb-3 leading-tight bg-transparent border-orange-400 focus:outline-none focus:shadow-outline'
+              onChange={(e) => setNewCustomer({ ...newCustomer, attendant: e.target.value })} value={newCustomer.attendant}
+              type='text' />
+          </div>
+          <div className='flex items-center justify-center '>
+            <button className=' text-gray-800 bg-orange-500 hover:text-orange-400 hover:bg-transparent hover:border border-orange-400 font-bold py-2 px-8 rounded ' type='button'>
+              Update
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
--- components/EditOffersForm.jsx
+++ components/EditOffersForm.jsx
@@ -2,6 +2,7 @@
 import React, { useState } from "react";
 import { useRouter } from "next/navigation";
 import { GrUpdate } from "react-icons/gr";
+import Image from "next/image";
 
 export default function EditOffersForm({ id, offer }) {
   const [newOffer, setNewOffer] = useState(offer);
@@ -28,52 +29,64 @@ export default function EditOffersForm({ id, offer }) {
   };
 
   return (
-    <div>
-      <div>
-        <h1 className="flex justify-center glow-text text-4xl font-thin p-10">
-          EditCustomersForm
-        </h1>
-        <div className=" flex justify-center items-center p-10">
-          <form
-            onSubmit={handleSubmit}
-            className=" flex flex-col justify-center rounded-lg px-4 py-2 bg-orange-200"
-          >
-            <div className="flex flex-col justify-center shadow-lg shadow-black rounded-lg px-4 py-2 m-4 ">
-              <label className="p-2 text-gray-800"> Title : </label>
-              <input
-                className="border border-gray-800 text-gray-800 rounded-lg px-6 py-2 m-2 bg-transparent"
-                onChange={(e) =>
-                  setNewOffer({ ...newOffer, title: e.target.value })
-                }
-                value={newOffer.title}
-              />
-              <label className="p-2  text-gray-800"> Description : </label>
-              <input
-                className="border border-gray-800 text-gray-800 rounded-lg px-6 py-2 m-2 bg-transparent"
-                onChange={(e) =>
-                  setNewOffer({ ...newOffer, description: e.target.value })
-                }
-                value={newOffer.description}
-                type="tel"
-                id="phone"
-                name="phone"
-              />
-              <label className="p-2  text-gray-800"> isActive :</label>
-              <input
-                className="border border-gray-800 text-gray-800 rounded-lg px-6 py-2 m-2 bg-transparent"
-                onChange={(e) =>
-                  setNewOffer({ ...newOffer, isActive: e.target.value })
-                }
-                value={newOffer.isActive}
-                type="checkbox"
-              />
-
-              <button className="mt-6 mb-4 px-4 py-2 rounded-md bg-orange-500 flex flex-row justify-center items-center rounded-badge">
-                <span className="mr-2">Update</span> <GrUpdate />{" "}
-              </button>
-            </div>
-          </form>
-        </div>
+    <div className='flex flex-col lg:flex-row h-screen'>
+      <div className='flex-1 flex flex-col items-center justify-center p-4 lg:p-10'>
+        <h1 className='text-4xl font-thin glow-text mb-4 lg:mb-6 p-6'> EDIT OFFERS</h1>
+        <form onSubmit={handleSubmit}
+          className='w-full max-w-sm backdrop-blur-0 shadow-[0_0_5px_0] p-6 lg:p-16 rounded-lg'>
+          <div className='mb-4'>
+            <label className='block text-orange-200 text-sm mb-2' htmlFor='tel'>
+              Title
+            </label>
+            <input
+              className='shadow border rounded w-full py-2 px-3 text-orange-400 leading-tight bg-transparent border-orange-400 focus:outline-none focus:shadow-outline'
+              onChange={(e) =>
+                setNewOffer({ ...newOffer, title: e.target.value })
+              }
+              value={newOffer.title}
+            />
+          </div>
+          <div className='mb-6'>
+            <label className='block text-orange-200 text-sm mb-2' htmlFor='text'>
+              Description :
+            </label>
+            <input className='shadow appearance-none border rounded w-full py-2 px-3 text-orange-400 mb-3 leading-tight bg-transparent border-orange-400 focus:outline-none focus:shadow-outline'
+              onChange={(e) =>
+                setNewOffer({ ...newOffer, description: e.target.value })
+              }
+              value={newOffer.description}
+              type="text"
+              id="text"
+              name="text"
+            />
+          </div>
+          <div>
+            <label className='block text-orange-200 text-sm mb-2' htmlFor='password'>
+              isActive :
+            </label>
+            <input className='mb-3 border-orange-400 focus:outline-none focus:shadow-outline'
+              onChange={(e) =>
+                setNewOffer({ ...newOffer, isActive: e.target.value })
+              }
+              value={newOffer.isActive}
+              type="checkbox"
+            />
+          </div>
+          <div className='flex items-center justify-center '>
+            <button className=' text-gray-800 bg-orange-500 hover:text-orange-400 hover:bg-transparent hover:border border-orange-400 font-bold py-2 px-8 rounded ' type='button'>
+              Update
+            </button>
+          </div>
+        </form>
+      </div>
+      <div className='flex-1 relative'>
+        <Image
+          src='/2.svg'
+          alt='404'
+          width={1080}
+          height={1920}
+          className='w-full h-full object-cover'
+        />
       </div>
     </div>
   );
