a92b642b006813b79623a8f1a71b873efb09ee55
Ruthviikthimmoji <149005776+ruthvikthimmoji@users.noreply.github.com>
2024-07-18 13:58:27 +0530

    offers edited

--- components/OffersList.jsx
+++ components/OffersList.jsx
@@ -2,39 +2,107 @@ import React from "react";
 import AddOffers from "./AddOffers";
 import Accordion from "./Accordian";
 import NavbarAll from "./NavbarAll";
+import * as Realm from "realm-web";
+import Link from 'next/link';
+import { useState, useEffect } from 'react';
+import DeleteBtn from "./DeleteBtn2";
+import EditBtn from "./EditBtn";
 
 const getOffers = async () => {
   try {
-    const res = await fetch("http://localhost:3000/api/offers", {
-      cache: "no-cache",
-    });
+    async function loginEmailPassword(email, password) {
+      const app = new Realm.App({ id: 'data-gacfoem' });
 
-    if (!res.ok) {
-      throw new Error("failed to fetch");
+      // Create an email/password credential
+      const credentials = Realm.Credentials.emailPassword(email, password);
+      // Authenticate the user
+      const user = await app.logIn(credentials);
+      // 'App.currentUser' updates to match the logged in user
+      console.assert(user.id === app.currentUser.id);
+      return user;
     }
 
+    const user = await loginEmailPassword('ruthvik@gmail.com', 'OxfMiQLGIXyKATl');
+    const res = await fetch('https://ap-south-1.aws.data.mongodb-api.com/app/data-gacfoem/endpoint/data/v1/action/find', {
+      method: 'POST',
+      headers: {
+        'Access-Control-Request-Headers': '*',
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + user.accessToken
+      },
+      cache: "no-cache",
+      body: JSON.stringify({
+        "collection": "offers",
+        "database": "offersDB",
+        "dataSource": "Cluster0",
+        "projection": {}
+      })
+    });
+    if(!res.ok){
+      throw new Error("Failed to fetch Offers");
+    }
     return res.json();
-  } catch (error) {
-    console.log("Error in loading", error);
-    return { offers: [] };
+  }catch (error) {
+    console.error("Error in Loading",error);
+    return{offers:[]};
   }
 };
+export default function OffrsList(){
+  // const [expandedOffer,setExpandedOffer] = useState(null);
+  const [offers,setoffers] = useState([]);
 
-export default async function OffersList() {
-  const { offers } = await getOffers();
+  const fetchOffers =async() =>{
+    const {documents} =await getOffers();
+    setoffers(documents);
+  };
 
-  return (
+  useEffect(()=>{
+    fetchOffers();
+  }, []);
+    
+
+  // const toggleOffer =(index) =>{
+  //   if(expandedOffer === index){
+  //     setExpandedOffer(null);
+  //   }else{
+  //     setExpandedOffer(index);
+  //   }
+  // };
+  return(
     <div>
       <NavbarAll/>
-      <h1 className="text-4xl font-thin flex flex-col justify-center items-center p-4 m-6 w-screen ">
-        OFFER DETAILS
+      <h1>
+        OFFER DEATILS
       </h1>
-      <div className="flex justify-end p-4">
-        <AddOffers />
+      <div>
+        <AddOffers/>
       </div>
-      <div className="p-4">
-        <Accordion offers={offers} />
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Description</th>
+            </tr>
+          </thead>
+          <tbody>
+            {offers && offers.map((f,index)=>(
+              <React.Fragment key = {f.id}>
+                <tr>
+                  <td>{f.title}</td>
+                  <td>{f.description}</td>
+                  <div>
+                    <DeleteBtn id = {f._id} />
+                    <Link href={`/editOffers/${f._id}`}>
+                    <EditBtn/>
+                    </Link>
+                  </div>
+                </tr>
+              </React.Fragment>
+            ))}
+          </tbody>
+        </table>
       </div>
     </div>
-  );
+  )
 }
